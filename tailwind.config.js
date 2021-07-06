25794c25b57187c38309c57b71b038415c95ac5f
Edmundo Perez <edmundo@michelada.io>
2021-07-06 11:52:15 -0500

    base typography added

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,33 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    fontFamily: { 
+      sans: ['Noto Sans'],
+      avant: ['AvantGarde']
+    },
+    extend: {
+      fontSize: { // https://type-scale.com/ using major third type scale
+        'lg': '1.25rem',
+        'xl': '1.563rem',
+        '2xl': '1.953rem',
+        '3xl': '2.441rem',
+        '4xl': '3.052rem',
+        '5xl': '3.815rem',
+        '6xl': '4.768rem',
+        '7xl': '5.96rem',
+        '8xl': '7.451rem',
+        '9xl': '9.313rem',
+      },
+      colors: {
+        gray: {
+          DEFAULT: '#575F65',
+        },
+        blue: {
+          dark: '#292C52',
+        },
+        red: '#ff0000',
+      }
+    },
   },
   variants: {
     extend: {},
