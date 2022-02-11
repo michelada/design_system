fd61e97a14ec9a63838626c8a04c6091e69b278f
Edmundo Perez <edmundo@michelada.io>
2022-02-11 11:42:10 -0600

    Stimulus addes, refactor tabs component

--- src/js/app.js
+++ src/js/app.js
@@ -13,6 +13,13 @@ import listPlugin from '@fullcalendar/list';
 
 import VanillaScrollspy from 'vanillajs-scrollspy';
 
+import { Application } from "@hotwired/stimulus"
+import { definitionsFromContext } from "@hotwired/stimulus-webpack-helpers"
+
+window.Stimulus = Application.start()
+const context = require.context("./controllers", true, /\.js$/)
+Stimulus.load(definitionsFromContext(context))
+
 document.addEventListener('DOMContentLoaded', function () {
     var calendarEl = document.getElementById('calendarWidget');
 
