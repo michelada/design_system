5829ae01bd686f6cf04bb8df148798a6ef62bc64
Edmundo Perez <edmundo@michelada.io>
2022-02-25 11:11:41 -0600

    navbar adjustments, increase package version

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   mode: 'jit',
-  purge: [
+  content: [
     './site/*.html',
   ],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: 'media',
   theme: {
     fontFamily: {
       sans: ['Noto Sans'],
