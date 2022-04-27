3b854b7cfcf234cd97629ede06a06779054802b9
Alejandro Rebollar <alejandro.rebollar@michelada.io>
2022-04-27 13:14:29 -0500

    Add new custom colors and remove unused old ones from tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,43 +13,31 @@ module.exports = {
       transparent: 'transparent',
       current: 'currentColor',
       black: '#000',
-      gray: {
-        DEFAULT: '#575F65',
-        light: '#828F96',
-        lighter: '#A5B1B8',
-        silver: '#D2D5DA',
-        lightest: '#E6EDF0',
-        ish: '#808C93',
+      primary: '#712384',
+      yellow: '#F1CF5A',
+      red: '#CA2438',
+      aqua: '#6DD6C4',
+      orange: '#ECAA32',
+      purple: {
+        DEFAULT: '#571F7C',
+        dark: '#38164E'
       },
-      blue: {
-        dark: '#292C52',
-        DEFAULT: '#3264FA',
-        crayola: '#245AF9',
-        picton: '#43B7E9',
-        wind: '#9CAFC8',
-        lavender: '#C1DBFC'
+      gray: {
+        DEFAULT: '#413E47',
+        light: '#666170',
+        lighter: '#8B8599',
+        silver: '#AAA5B8',
+        lightest: '#EBE6F0',
+        ish: '#F8F5FA'
       },
       white: {
         DEFAULT: '#FFFFFF',
-        ish: '#F5F8FA',
-        antiflash: "#EAF1F9",
-        mystic: '#E6EDF0'
+        ish: '#FAFAFA'
       },
-      dark: {
-        DEFAULT: '#15162F',
-        charcoal: '#333333',
-      },
-      yellow: {
-        DEFAULT: '#F9D026',
-        darker: '#FFC800'
-      },
-      red: '#EA3434',
-      green: '#00B5AF',
       fullcalendar: {
         "event-1": 'var(--fullcalendar-event-1)',
         "event-2": 'var(--fullcalendar-event-2)',
-        "event-3": 'var(--fullcalendar-event-3)',
-        "event-4": 'var(--fullcalendar-event-4)',
+        "event-3": 'var(--fullcalendar-event-3)'
       }
     },
     extend: {
@@ -72,6 +60,10 @@ module.exports = {
         '7xl': '5.96rem',
         '8xl': '7.451rem',
         '9xl': '9.313rem',
+      },
+      colors: {
+        'norden-violet': '#712384',
+        'norden-purple': '#38164E'
       },
     },
   },
