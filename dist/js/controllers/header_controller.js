cbcf7cfa6129fcc0f2675700acfe0c7a6ccce016
Alejandro Rebollar <alejandro.rebollar@michelada.io>
2022-04-28 16:53:41 -0500

    Compile js to dist folder

---
+++ dist/js/controllers/header_controller.js
@@ -0,0 +1,22 @@
+import { Controller } from "@hotwired/stimulus"
+export default class extends Controller {
+  static targets = ["menu", "close", "avatar", "submenu"]
+
+  connect () {
+    if(window.innerWidth <= 768) {
+      this.menuTarget.classList.toggle("hidden");
+      this.submenuTarget.classList.toggle("hidden");
+    }
+  }
+  
+  toggleSubmenu(e) {
+    e.preventDefault();
+    this.submenuTarget.classList.toggle("hidden");
+  }
+
+  toggleMenu() {
+    this.menuTarget.classList.toggle("hidden");
+    this.closeTarget.classList.toggle("hidden");
+    this.avatarTarget.classList.toggle("hidden");
+  }
+}
